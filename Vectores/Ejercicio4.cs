b2d500819890bb245199e03ff0f638ff2d11d3b2
myusername <myemail>
2023-03-10 14:59:49 -0500

    ejercicio4

---
+++ Vectores/Ejercicio4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Arrglos.Vectores
+{
+    internal class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[10];
+
+            Array.Sort(vector);
+
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = i;
+                Console.WriteLine(vector[i]);
+            }
+        }
+    }
+}
